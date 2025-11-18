2f07fc67b1d78b000c56f1f0e2d06ee058419ecc
Francisco Silva <francisco.silva@huemulsolutions.com>
2025-11-17 23:51:25 -0300

    feat: agregar exportaciones de ContextSheet, TemplateConfigSheet y ExecutionInfoSheet en el índice de hojas

--- src/components/sheets/index.ts
+++ src/components/sheets/index.ts
@@ -2,3 +2,5 @@ export { ExecuteSheet } from "./ExecuteSheet";
 export { SectionSheet } from "./SectionSheet";
 export { DependenciesSheet } from "./DependenciesSheet";
 export { ContextSheet } from "./ContextSheet";
+export { TemplateConfigSheet } from "./TemplateConfigSheet";
+export { ExecutionInfoSheet } from "./ExecutionInfoSheet";
