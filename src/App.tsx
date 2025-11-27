93ed9a89f057d748152acf1e69697ac0c13c55a9
Francisco Silva <francisco.silva@huemulsolutions.com>
2025-11-27 11:26:12 -0300

    feat: envolver rutas en AuthProvider y ProtectedRoute para gestión de autenticación

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context";
+import { ProtectedRoute } from "./components/auth/protected-route";
 import AppLayout from "./components/layout/app-layout";
 import Home from "./pages/home";
 import Templates from "./pages/templates";
@@ -16,24 +18,28 @@ import ModelsPage from "./pages/models";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<Navigate to="home" replace />} />
-        <Route path="home" element={<Home />} />
-        <Route path="organizations" element={<Organizations />} />
-        <Route path="templates" element={<Templates />} />
-        <Route path="templates/:id" element={<Templates />} />
-        <Route path="search" element={<SearchPage />} />
-        <Route path="asset" element={<Assets />} />
-        <Route path="asset/*" element={<Assets />} />
-        <Route path="graph" element={<Graph />} />
-        <Route path="models" element={<ModelsPage />} />
-        <Route path="configTemplate/:id" element={<ConfigTemplate />} />
-        <Route path="document/:id" element={<DocumentPage />} />
-        <Route path="configDocument/:id" element={<ConfigDocumentPage />} />
-        <Route path="execution/:id" element={<ExecutionPage />} />
-        <Route path="docDepend/:id" element={<DocDependPage />} />
-      </Route>
-    </Routes>
+    <AuthProvider>
+      <ProtectedRoute>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Navigate to="home" replace />} />
+            <Route path="home" element={<Home />} />
+            <Route path="organizations" element={<Organizations />} />
+            <Route path="templates" element={<Templates />} />
+            <Route path="templates/:id" element={<Templates />} />
+            <Route path="search" element={<SearchPage />} />
+            <Route path="asset" element={<Assets />} />
+            <Route path="asset/*" element={<Assets />} />
+            <Route path="graph" element={<Graph />} />
+            <Route path="models" element={<ModelsPage />} />
+            <Route path="configTemplate/:id" element={<ConfigTemplate />} />
+            <Route path="document/:id" element={<DocumentPage />} />
+            <Route path="configDocument/:id" element={<ConfigDocumentPage />} />
+            <Route path="execution/:id" element={<ExecutionPage />} />
+            <Route path="docDepend/:id" element={<DocDependPage />} />
+          </Route>
+        </Routes>
+      </ProtectedRoute>
+    </AuthProvider>
   );
 }
