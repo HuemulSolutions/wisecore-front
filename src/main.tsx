d15828f43d6ef9b37024aba31a2589499466c44f
Francisco Silva <francisco.silva@huemulsolutions.com>
2026-02-23 17:12:12 -0300

    feat: eliminar el proveedor de organización del árbol de componentes

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { OrganizationProvider } from './contexts/organization-context.ts';
+
 import { Toaster } from "@/components/ui/sonner"
 import { ApiError } from '@/types/api-error';
 import { handleApiError } from '@/lib/error-utils';
@@ -112,12 +112,10 @@ console.error = function(...args) {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <OrganizationProvider>
       <Toaster richColors />
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      </OrganizationProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
